3d36a09c21422b300c16a9ded9a59169d833ec6e
Peng Xu <robotpengxu@gmail.com>
2022-03-16 21:44:32 -0700

    Custumed scenarios: stop sign, urban single lane.

---
+++ srunner/scenarios/casper_auto_stop_sign.py
@@ -0,0 +1,259 @@
+#!/usr/bin/env python
+
+# Copyright (c) 2018-2020 Intel Corporation
+#
+# This work is licensed under the terms of the MIT license.
+# For a copy, see <https://opensource.org/licenses/MIT>.
+
+"""
+Stop sign scenario:
+
+TODO: description.
+"""
+
+import random
+import math
+import py_trees
+import carla
+
+from srunner.scenariomanager.carla_data_provider import CarlaDataProvider
+from srunner.scenariomanager.scenarioatomics.atomic_behaviors import (ActorTransformSetter,
+                                                                      ActorDestroy,
+                                                                      KeepVelocity,
+                                                                      StopVehicle,
+                                                                      WaypointFollower)
+from srunner.scenariomanager.scenarioatomics.atomic_criteria import CollisionTest, RunningStopTest
+from srunner.scenariomanager.scenarioatomics.atomic_trigger_conditions import (InTriggerDistanceToVehicle,
+                                                                               InTriggerDistanceToNextIntersection,
+                                                                               InTriggerDistanceToLocation,
+                                                                               InTriggerDistanceToLocationAlongRoute,
+                                                                               DriveDistance,
+                                                                               StandStill)
+from srunner.scenariomanager.timer import TimeOut
+from srunner.scenarios.basic_scenario import BasicScenario
+from srunner.tools.scenario_helper import get_location_in_distance_from_wp, get_waypoint_in_distance
+
+
+class CasperAutoStopSign(BasicScenario):
+
+    """
+    This class holds everything required for a simple "Stop sign"
+    scenario.
+
+    This is a single ego vehicle scenario
+    """
+
+    timeout = 120            # Timeout of scenario in seconds
+
+    def __init__(self, world, ego_vehicles, config, randomize=False, debug_mode=False, criteria_enable=True,
+                 timeout=60):
+        """
+        Setup all relevant parameters and create scenario
+
+        If randomize is True, the scenario parameters are randomized
+        """
+
+        self.timeout = timeout
+        self._map = CarlaDataProvider.get_map()
+        self._reference_waypoint = self._map.get_waypoint(config.trigger_points[0].location)
+        self._transform = None
+
+        super(CasperAutoStopSign, self).__init__("CasperAutoStopSign",
+                                                  ego_vehicles,
+                                                  config,
+                                                  world,
+                                                  debug_mode,
+                                                  criteria_enable=criteria_enable)
+
+        if randomize:
+            self._ego_other_distance_start = random.randint(4, 8)
+
+            # Example code how to randomize start location
+            # distance = random.randint(20, 80)
+            # new_location, _ = get_location_in_distance(self.ego_vehicles[0], distance)
+            # waypoint = CarlaDataProvider.get_map().get_waypoint(new_location)
+            # waypoint.transform.location.z += 39
+            # self.other_actors[0].set_transform(waypoint.transform)
+
+    def _initialize_actors(self, config):
+        """
+        Custom initialization
+        """
+
+        pass
+
+    def _create_behavior(self):
+        """
+        The scenario defined after is a "follow leading vehicle" scenario. After
+        invoking this scenario, it will wait for the user controlled vehicle to
+        enter the start region, then make the other actor to drive until reaching
+        the next intersection. Finally, the user-controlled vehicle has to be close
+        enough to the other actor to end the scenario.
+        If this does not happen within 60 seconds, a timeout stops the scenario
+        """
+
+        # TODO: Hardcoded location for now.
+        target_location = carla.Location(203.0, -206.0)
+
+        # wait at stop sign
+        wait_for_stop_sign = StandStill(self.ego_vehicles[0], name="StopSign", duration=3)
+
+        # end condition
+        endcondition = py_trees.composites.Parallel("Waiting for end position",
+                                                    policy=py_trees.common.ParallelPolicy.SUCCESS_ON_ALL)
+        endcondition_part1 = InTriggerDistanceToLocation(self.ego_vehicles[0],
+                                                         target_location,
+                                                         distance=5,
+                                                         name="TargetLocationReached")
+        endcondition_part2 = StandStill(self.ego_vehicles[0], name="StandStill", duration=1)
+        endcondition.add_child(endcondition_part1)
+        endcondition.add_child(endcondition_part2)
+
+        # Build behavior tree
+        sequence = py_trees.composites.Sequence("Sequence Behavior")
+        sequence.add_child(endcondition)
+
+        return sequence
+
+    def _create_test_criteria(self):
+        """
+        A list of all test criteria will be created that is later used
+        in parallel behavior tree.
+        """
+        criteria = []
+
+        collision_criterion = CollisionTest(self.ego_vehicles[0])
+        stop_sign_criterion = RunningStopTest(self.ego_vehicles[0])
+
+        criteria.append(collision_criterion)
+        criteria.append(stop_sign_criterion)
+
+        return criteria
+
+    def __del__(self):
+        """
+        Remove all actors upon deletion
+        """
+        self.remove_all_actors()
+
+
+class CasperAutoStopSignWithObstacle(BasicScenario):
+
+    """
+    This class holds everything required for a simple "Follow a leading vehicle"
+    scenario involving two vehicles.  (Traffic Scenario 2)
+
+    This is a single ego vehicle scenario
+    """
+
+    timeout = 120            # Timeout of scenario in seconds
+
+    def __init__(self, world, ego_vehicles, config, randomize=False, debug_mode=False, criteria_enable=True,
+                 timeout=60):
+        """
+        Setup all relevant parameters and create scenario
+
+        If randomize is True, the scenario parameters are randomized
+        """
+
+        self.timeout = timeout
+        self._map = CarlaDataProvider.get_map()
+        self._reference_waypoint = self._map.get_waypoint(config.trigger_points[0].location)
+        self._transform = None
+
+        self._stopped_vehicle_location = 15
+
+        super(CasperAutoStopSignWithObstacle, self).__init__("CasperAutoStopSignWithObstacle",
+                                                             ego_vehicles,
+                                                             config,
+                                                             world,
+                                                             debug_mode,
+                                                             criteria_enable=criteria_enable)
+
+        if randomize:
+            self._ego_other_distance_start = random.randint(4, 8)
+
+            # Example code how to randomize start location
+            # distance = random.randint(20, 80)
+            # new_location, _ = get_location_in_distance(self.ego_vehicles[0], distance)
+            # waypoint = CarlaDataProvider.get_map().get_waypoint(new_location)
+            # waypoint.transform.location.z += 39
+            # self.other_actors[0].set_transform(waypoint.transform)
+
+    def _initialize_actors(self, config):
+        """
+        Custom initialization
+        """
+
+        # add actors from xml file
+        for actor in config.other_actors:
+            vehicle = CarlaDataProvider.request_new_actor(actor.model, actor.transform)
+            self.other_actors.append(vehicle)
+            # vehicle.set_simulate_physics(enabled=False)
+
+        stopped_vehicle_waypoint, _ = get_waypoint_in_distance(self._reference_waypoint, self._stopped_vehicle_location)
+        self._other_actor_transform = carla.Transform(
+            carla.Location(stopped_vehicle_waypoint.transform.location.x,
+                           stopped_vehicle_waypoint.transform.location.y - 1.3,
+                           stopped_vehicle_waypoint.transform.location.z + 1),
+            stopped_vehicle_waypoint.transform.rotation)
+        stopped_vehicle_transform = carla.Transform(
+            carla.Location(self._other_actor_transform.location.x,
+                           self._other_actor_transform.location.y - 1.3,
+                           self._other_actor_transform.location.z + 1),
+            self._other_actor_transform.rotation)
+
+    def _create_behavior(self):
+        """
+        If this does not happen within 60 seconds, a timeout stops the scenario
+        """
+
+        # TODO: Hardcoded location for now.
+        target_location = carla.Location(203.0, -206.0)
+
+        # to avoid the other actor blocking traffic, it was spawed elsewhere
+        # reset its pose to the required one
+        stopped_vehicle_transform = ActorTransformSetter(self.other_actors[0], self._other_actor_transform)
+
+        # wait at stop sign
+        wait_for_stop_sign = StandStill(self.ego_vehicles[0], name="StopSign", duration=3)
+
+        # end condition
+        endcondition = py_trees.composites.Parallel("Waiting for end position",
+                                                    policy=py_trees.common.ParallelPolicy.SUCCESS_ON_ALL)
+        endcondition_part1 = InTriggerDistanceToLocation(self.ego_vehicles[0],
+                                                         target_location,
+                                                         distance=5,
+                                                         name="TargetLocationReached")
+        endcondition_part2 = StandStill(self.ego_vehicles[0], name="StandStill", duration=1)
+        endcondition.add_child(endcondition_part1)
+        endcondition.add_child(endcondition_part2)
+
+        # Build behavior tree
+        sequence = py_trees.composites.Sequence("Sequence Behavior")
+        # sequence.add_child(stopped_vehicle_transform)
+        sequence.add_child(endcondition)
+        sequence.add_child(ActorDestroy(self.other_actors[0]))
+
+        return sequence
+
+    def _create_test_criteria(self):
+        """
+        A list of all test criteria will be created that is later used
+        in parallel behavior tree.
+        """
+        criteria = []
+
+        collision_criterion = CollisionTest(self.ego_vehicles[0])
+        stop_sign_criterion = RunningStopTest(self.ego_vehicles[0])
+
+        criteria.append(collision_criterion)
+        criteria.append(stop_sign_criterion)
+
+        return criteria
+
+    def __del__(self):
+        """
+        Remove all actors upon deletion
+        """
+        self.remove_all_actors()
